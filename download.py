cf7e3f63d198e1c0a3d7368cace503689cf18f7c
Fahad <43470415+bperl03@users.noreply.github.com>
2020-04-17 21:41:40 +0300

    Delete download.py

--- download.py
+++
@@ -1,7 +0,0 @@
-import urllib
-print('Downloading file')
-#x = input('Enter Download Link > ')
-x = input('Enter file link > ')
-c = str(input('input file name > '))
-urllib.urlretrieve(x,str(c))
-print('File Downloaded')
