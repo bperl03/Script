91e302e8465b30b0268fde44d70f0c0538a9539e
bperl03 <bperl03@gmail.com>
2020-04-18 03:31:58 +0300

    Update the download script

--- Dowanload.py
+++ Dowanload.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
+# Download script
 import urllib.request
-url = input('Enter file link > ')
-File_name = str(input('input file name > '))
+url = input('Enter file link > ') # the file Url
+File_name = str(input('input file name > ')) #file name to save it with new name
 print('Downloading file')
 with urllib.request.urlretrieve(url, File_name) as down :
     down()
